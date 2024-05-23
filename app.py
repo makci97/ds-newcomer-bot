9a588932100c06cb218ab8ab518db67d316a25ef
Nikita Trubkin <trubkin.nikita@yandex.ru>
2024-05-23 20:03:22 +0300

    Add some commands to command tree (#4)
    * Add some commands to the command tree
    
    * Fix linting issues
    
    * Fix linting issues

--- app.py
+++ app.py
@@ -1,16 +1,144 @@
-from telegram.ext import CommandHandler, MessageHandler, filters
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardRemove, Update
+from telegram.ext import CallbackContext, CallbackQueryHandler, CommandHandler, ConversationHandler
 
 from config.telegram_bot import application
-from handlers.command_handlers import start_reply
-from handlers.message_handlers import chatgpt_reply
+from exceptions.bad_argument_error import BadArgumentError
+from exceptions.bad_choice_error import BadChoiceError
 
-# Регистрация обработчика текстовых сообщений
-message_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, chatgpt_reply)
-application.add_handler(message_handler)
+# Define states
+TASK_CHOICE, KNOWLEDGE_GAIN, INTERVIEW_PREP, PROBLEM_SOL, CODE_EXPL, CODE_WRITING, PROBLEM_HELP, EDA, MEME_EXPL = range(
+    9,
+)
 
-# Регистрация обработчика команд
-start_command_handler = CommandHandler("start", start_reply)
-application.add_handler(start_command_handler)
+CALLBACK_QUERY_ARG = "update.callback_query"
+MESSAGE_ARG = "update.message"
+
+
+async def start(update: Update, _: CallbackContext) -> int:
+    """Начальный хэндлер дерева команд."""
+    keyboard = [
+        [InlineKeyboardButton("Прокачка знаний", callback_data="KNOWLEDGE_GAIN")],
+        [InlineKeyboardButton("Помоги решить задачу", callback_data="PROBLEM_SOL")],
+        [InlineKeyboardButton("Oбъясни IT мем", callback_data="MEME_EXPL")],
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    if update.message:  # When /start command is used
+        await update.message.reply_text("Выберите задачу:", reply_markup=reply_markup)
+    elif update.callback_query:
+        await update.callback_query.edit_message_text("Please choose:", reply_markup=reply_markup)
+    return TASK_CHOICE
+
+
+async def task_choice(update: Update, _: CallbackContext) -> int:
+    """хэндлер выбора базовой группы задач."""
+    query = update.callback_query
+    if query is None:
+        raise BadArgumentError(CALLBACK_QUERY_ARG)
+    await query.answer()
+    choice = query.data
+
+    if choice == "KNOWLEDGE_GAIN":
+        keyboard = [
+            [InlineKeyboardButton("Подготовка к собесу", callback_data="INTERVIEW_PREP")],
+            [InlineKeyboardButton("Назад", callback_data="BACK")],
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        await query.edit_message_text(text="Ты выбрал прокачку знаний:", reply_markup=reply_markup)
+        return KNOWLEDGE_GAIN
+    if choice == "PROBLEM_SOL":
+        keyboard = [
+            [InlineKeyboardButton("Объясни код", callback_data="CODE_EXPL")],
+            [InlineKeyboardButton("Напиши код", callback_data="CODE_WRITING")],
+            [InlineKeyboardButton("Помоги решить задачу", callback_data="PROBLEM_HELP")],
+            [InlineKeyboardButton("EDA датасета", callback_data="EDA")],
+            [InlineKeyboardButton("Назад", callback_data="BACK")],
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        await query.edit_message_text(text="Помощь в решении задачи:", reply_markup=reply_markup)
+        return PROBLEM_SOL
+    if choice == "MEME_EXPL":
+        if update.callback_query is None:
+            raise BadArgumentError(CALLBACK_QUERY_ARG)
+        await update.callback_query.edit_message_text(text="Скоро мы научимся объяснять мемы. Беседа завершена.")
+        return ConversationHandler.END
+    raise BadChoiceError(choice)
+
+
+async def knowledge_gain(update: Update, context: CallbackContext) -> int:
+    """хэндлер выбора прокачки знаний."""
+    query = update.callback_query
+    if query is None:
+        raise BadArgumentError(CALLBACK_QUERY_ARG)
+    await query.answer()
+    choice = query.data
+    if choice == "INTERVIEW_PREP":
+        await query.edit_message_text(text="Подготовка к собесу скоро будет доступна. Беседа завершена.")
+        return ConversationHandler.END
+    if choice == "BACK":
+        return await start(update, context)
+    raise BadChoiceError(choice)
+
+
+async def problem_solving(update: Update, context: CallbackContext) -> int:
+    """хэндлер выбора помощи в решении задач."""
+    query = update.callback_query
+    if query is None:
+        raise BadArgumentError(CALLBACK_QUERY_ARG)
+    await query.answer()
+    choice = query.data
+    if choice == "CODE_EXPL":
+        await code_explanation(update, context)
+    if choice == "CODE_WRITING":
+        if update.callback_query is None:
+            raise BadArgumentError(CALLBACK_QUERY_ARG)
+        await update.callback_query.edit_message_text(text="Скоро мы научимся писать код. Беседа завершена.")
+        return ConversationHandler.END
+    if choice == "PROBLEM_HELP":
+        if update.callback_query is None:
+            raise BadArgumentError(CALLBACK_QUERY_ARG)
+        await update.callback_query.edit_message_text(
+            text="Скоро мы научимся помогать в решении задач. Беседа завершена.",
+        )
+        return ConversationHandler.END
+    if choice == "EDA":
+        if update.callback_query is None:
+            raise BadArgumentError(CALLBACK_QUERY_ARG)
+        await update.callback_query.edit_message_text(text="Скоро мы научимся EDA. Беседа завершена.")
+        return ConversationHandler.END
+    if choice == "BACK":
+        return await start(update, context)
+    raise BadChoiceError(choice)
+
+
+async def code_explanation(update: Update, _: CallbackContext) -> int:
+    """Хэндлер выбора объяснения кода."""
+    if update.callback_query is None:
+        raise BadArgumentError(CALLBACK_QUERY_ARG)
+    await update.callback_query.edit_message_text(text="Скоро мы научимся объяснять код. Беседа завершена.")
+    return ConversationHandler.END
+
+
+async def cancel(update: Update, _: CallbackContext) -> int:
+    """Завершает беседу."""
+    if update.message is None:
+        raise BadArgumentError(MESSAGE_ARG)
+    await update.message.reply_text("Беседа завершена.", reply_markup=ReplyKeyboardRemove())
+    return ConversationHandler.END
+
+
+"""Run the bot."""
+conv_handler = ConversationHandler(
+    entry_points=[CommandHandler("start", start)],
+    states={
+        TASK_CHOICE: [CallbackQueryHandler(task_choice)],
+        KNOWLEDGE_GAIN: [CallbackQueryHandler(knowledge_gain)],
+        PROBLEM_SOL: [CallbackQueryHandler(problem_solving)],
+    },
+    fallbacks=[CommandHandler("cancel", cancel)],
+)
+
+application.add_handler(conv_handler)
+application.add_handler(CommandHandler("start", start))
 
 # Запуск бота
 application.run_polling()
