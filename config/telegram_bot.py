4e4f86fb3e70754d02167d1543354d9fbd13dcff
ntrubkin <trubkin.nikita@yandex.ru>
2024-05-24 00:07:18 +0300

    Add /start button

--- config/telegram_bot.py
+++ config/telegram_bot.py
@@ -2,5 +2,10 @@ from telegram.ext import Application
 
 from .tokens import TELEGRAM_BOT_TOKEN
 
+
+async def post_init(app: Application) -> None:
+    await app.bot.set_my_commands([('start', 'Запускает бота')])
+
+
 # Создание экземпляра бота
-application: Application = Application.builder().token(TELEGRAM_BOT_TOKEN).build()
+application: Application = Application.builder().token(TELEGRAM_BOT_TOKEN).post_init(post_init).build()
