925599490ed261ba323bdae8edef0c5d8f9b06fa
ntrubkin <trubkin.nikita@yandex.ru>
2024-05-24 12:35:19 +0300

    Add meme explanation

--- config/telegram_bot.py
+++ config/telegram_bot.py
@@ -4,7 +4,8 @@ from .tokens import TELEGRAM_BOT_TOKEN
 
 
 async def post_init(app: Application) -> None:
-    await app.bot.set_my_commands([('start', 'Запускает бота')])
+    """Донастраивает бота после старта."""
+    await app.bot.set_my_commands([("start", "Запускает бота")])
 
 
 # Создание экземпляра бота
